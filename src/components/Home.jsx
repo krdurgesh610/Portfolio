edf84ab280b350ca9f4f70874902ff8dccf3e89c
krdurgesh610 <kr.durgesh610@gmail.com>
2024-07-07 12:54:55 +0530

    My Portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
             I am Durgesh Kumar Thakur.
           </h2>
           <h3 className="text-2xl sm:text-4xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Java Full Stack Developer.
           </h3>
           <div>
             <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
